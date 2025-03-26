caa65b04ebaae35aae4052d7a3846fb45e13e211
paieer <paieer@users.noreply.github.com>
2025-03-26 18:14:27 +0800

    update auth

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,11 @@
+import { config } from "dotenv"
 import { Hono } from "hono"
+import { bearerAuth } from "hono/bearer-auth"
 import { cors } from "hono/cors"
 import { logger } from "hono/logger"
+import { randomBytes } from "node:crypto"
+import { writeFileSync } from "node:fs"
+import { join } from "node:path"
 
 import { completionRoutes } from "./routes/chat-completions/route"
 import { embeddingRoutes } from "./routes/embeddings/route"
@@ -8,11 +13,20 @@ import { modelRoutes } from "./routes/models/route"
 
 export const server = new Hono()
 
+const envFilePath = join(process.cwd(), ".env")
+config()
+const API_KEY = process.env.API_KEY || randomBytes(20).toString("hex")
+if (!process.env.API_KEY) {
+  writeFileSync(envFilePath, `API_KEY=${API_KEY}\n`)
+}
+console.log(`Current API_KEY: ${API_KEY}`)
+
 server.use(logger())
 server.use(cors())
 
 server.get("/", (c) => c.text("Server running"))
 
+server.use(bearerAuth({ token: API_KEY }))
 server.route("/chat/completions", completionRoutes)
 server.route("/models", modelRoutes)
 server.route("/embeddings", embeddingRoutes)
